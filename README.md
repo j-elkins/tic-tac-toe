0cc3136a239b641a7d1fcf3ae77e30b3c2a48703
Jasmine Elkins <jasmine.elkins@gmail.com>
2021-12-09 19:37:48 -0500

    Styling, background, starting to write logic to check object

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Tic-Tac-Toe Game
 
 Created in November 2021 as an assignment for The Odin Project Full Stack Web Developer Course.
+
+Background image:
+<a href="https://www.vecteezy.com/free-vector/geometric-background">Geometric Background Vectors by Vecteezy</a>
--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const GameboardArray = {};
 
 // players click buttons with X or O to play their turn
 let playerIcon = "";
+let iconStyle = "";
 
 const xButton = document.querySelector("#X");
 xButton.addEventListener("click", (e) => {
-  playerIcon = "x";
-  console.log(e);
+  playerIcon = "X";
+  iconStyle =
+    "text-shadow: 0 0 4px rgb(210, 1, 116), 0 0 7px rgb(255, 0, 174);";
 });
 
 const oButton = document.querySelector("#O");
 oButton.addEventListener("click", (e) => {
-  playerIcon = "o";
-  console.log(e);
+  playerIcon = "O";
+  iconStyle =
+    "text-shadow: 0 0 4px rgb(250, 129, 0), 0 0 7px rgb(255, 183, 0), 0 0 7px rgb(255, 183, 0)";
 });
 
 // select a square, click to play turn
@@ -26,15 +29,51 @@ gameboard.addEventListener("click", (event) => {
 
   if (squareClicked.textContent == "") {
     squareClicked.textContent = playerIcon;
+    squareClicked.style.cssText = iconStyle;
     GameboardArray[squareClicked.id] = playerIcon;
   }
 
   console.log({ GameboardArray });
+  // checkForWinner();
 });
 
 // after each turn check object to see if 3 in a row contain the same
 function checkForWinner() {
   for (cell in GameboardArray) {
+    // check each row: if first cell not empty, move to next column a2
+    if (a1 !== "") {
+      if (a2 !== "") {
+        if (a3 !== "") {
+          // check if a1, a2, a3 have the same mark
+          // if (a1 === a2 === a3) {
+          congratulateWinner();
+          // } // else move on to b1
+        }
+      } // else if a2 empty, move to b1
+      // if a1 empty, move to next row b1
+    } else if (b1 !== "") {
+      if (b2 !== "") {
+        if (b3 !== "") {
+          // check if b1, b2, b3 have the same mark
+          // if (b1 === b2 === b3) {
+          congratulateWinner();
+          // } // else move on to c1
+        }
+      } // else if b2 empty, move to c1
+      // if b1 empty, move to next row c1
+    } else if (c1 !== "") {
+      if (c2 !== "") {
+        if (c3 !== "") {
+          // check if c1, c2, c3 have the same mark
+          // if (c1 === c2 === c3) {
+          congratulateWinner();
+          // } // else back to a1 and check each column:
+        }
+      }
+    }
+
+    if (a1) {
+    }
   }
 }
 
