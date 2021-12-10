de797f3517ffd6ffa5bdf743a12fa8a3cd73cbf8
Jasmine Elkins <jasmine.elkins@gmail.com>
2021-12-10 11:08:24 -0500

    Winner announced!

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ let GameboardArray = {};
 
 // players click buttons with X or O to play their turn
 let playerIcon = "";
+let playerName = "";
 let iconClass = "";
 
 const xButton = document.querySelector("#xBtn");
 xButton.addEventListener("click", (e) => {
   playerIcon = "X";
+  playerName = "Player One";
   iconClass = "playerOneIconStyle";
 });
 
 const oButton = document.querySelector("#oBtn");
 oButton.addEventListener("click", (e) => {
   playerIcon = "O";
+  playerName = "Player Two";
   iconClass = "playerTwoIconStyle";
 });
 
@@ -25,22 +28,16 @@ gameboard.addEventListener("click", (event) => {
   if (squareClicked.textContent == "") {
     squareClicked.textContent = playerIcon;
     squareClicked.classList.add(iconClass);
-    GameboardArray[squareClicked.id] = playerIcon;
+    GameboardArray[squareClicked.id] = playerName;
   }
 
   console.log({ GameboardArray });
   checkForWinner();
 });
 
-// hardcode to test...
-
-// GameboardArray = {
-//   A0: "X",
-//   B0: "X",
-//   C0: "X",
-// };
-
 // after each turn check object to see if 3 in a row contain the same
+let playerWhoWon = "";
+
 function checkForWinner() {
   // check for winners in rows
   if (
@@ -48,7 +45,9 @@ function checkForWinner() {
     GameboardArray["A0"] == GameboardArray["A1"] &&
     GameboardArray["A0"] == GameboardArray["A2"]
   ) {
+    playerWhoWon = GameboardArray["A0"];
     console.log(GameboardArray["A0"] + "is the winner");
+    announceWinner(playerWhoWon);
   }
 
   if (
@@ -56,7 +55,9 @@ function checkForWinner() {
     GameboardArray["B0"] == GameboardArray["B1"] &&
     GameboardArray["B0"] == GameboardArray["B2"]
   ) {
+    playerWhoWon = GameboardArray["B0"];
     console.log(GameboardArray["B0"] + "is the winner");
+    announceWinner(playerWhoWon);
   }
 
   if (
@@ -64,7 +65,9 @@ function checkForWinner() {
     GameboardArray["C0"] == GameboardArray["C1"] &&
     GameboardArray["C0"] == GameboardArray["C2"]
   ) {
+    playerWhoWon = GameboardArray["C0"];
     console.log(GameboardArray["C0"] + "is the winner");
+    announceWinner(playerWhoWon);
   }
 
   // check for winners in columns
@@ -73,7 +76,9 @@ function checkForWinner() {
     GameboardArray["A0"] == GameboardArray["B0"] &&
     GameboardArray["C0"]
   ) {
+    playerWhoWon = GameboardArray["A0"];
     console.log(GameboardArray["A0"] + " is the winner");
+    announceWinner(playerWhoWon);
   }
 
   if (
@@ -81,7 +86,9 @@ function checkForWinner() {
     GameboardArray["A1"] == GameboardArray["B1"] &&
     GameboardArray["A1"] == GameboardArray["C1"]
   ) {
+    playerWhoWon = GameboardArray["A1"];
     console.log(GameboardArray["A1"] + " is the winner");
+    announceWinner(playerWhoWon);
   }
 
   if (
@@ -89,7 +96,9 @@ function checkForWinner() {
     GameboardArray["A2"] == GameboardArray["B2"] &&
     GameboardArray["A2"] == GameboardArray["C2"]
   ) {
+    playerWhoWon = GameboardArray["A2"];
     console.log(GameboardArray["A2"] + " is the winner");
+    announceWinner(playerWhoWon);
   }
 
   // check for winner diagonally
@@ -98,7 +107,9 @@ function checkForWinner() {
     GameboardArray["A0"] == GameboardArray["B1"] &&
     GameboardArray["A0"] == GameboardArray["C2"]
   ) {
-    console.log(GameboardArray["A0"] + "is the winner");
+    playerWhoWon = GameboardArray["A0"];
+    console.log(GameboardArray["A0"] + " is the winner");
+    announceWinner(playerWhoWon);
   }
 
   if (
@@ -106,16 +117,35 @@ function checkForWinner() {
     GameboardArray["C0"] == GameboardArray["B1"] &&
     GameboardArray["C0"] == GameboardArray["A2"]
   ) {
-    console.log(GameboardArray["C0"] + "is the winner");
+    playerWhoWon = GameboardArray["C0"];
+    console.log(GameboardArray["C0"] + " is the winner");
+    announceWinner(playerWhoWon);
   }
 }
 
-checkForWinner();
-// check each row: if first cell is empty, move to next row b0
+function itsATie() {
+  // if board contains values in all 9 squares and there's no winner
+  // announceTie()
+}
 
 // if 3 consecutive squares contain the same, game ends
+function announceWinner(winner) {
+  const announcement = document.querySelector("#choices");
+  announcement.textContent = "Congratulations! " + winner + " is the winner";
+  announcement.classList.add("announcement");
 
-// congratulate the winner (or tie)
+  if (winner == "Player One") {
+    announcement.classList.add("playerOneWins");
+    gameboard.classList.add("playerOneWins");
+  }
+
+  if (winner == "Player Two") {
+    announcement.classList.add("playerTwoWins");
+    gameboard.classList.add("playerTwoWins");
+  }
+}
+
+// if all squares filled but no winner, announce tie
 
 // click 'start' button to begin
 // players enter names into input field (or prompt?)
